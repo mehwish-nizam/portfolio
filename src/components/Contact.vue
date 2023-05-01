fa5e935fe0f94c3fcfb4a1c6e45b9c154f7b094e
mehwish <mehwishnizam01@gmail.com>
2023-05-01 23:18:49 +0500

    Add alerts to enhance user experience

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -12,7 +12,14 @@
             <input type="email" class="email" required placeholder="Enter your email" v-model="email">
             <input  class="message" required placeholder="Write your message" v-model="message"/>
             <button class="send-btn" type="submit">
-                Send Message
+                <transition name="v-btn-label" mode="out-in">
+                    <div v-if="loading">
+                        <span class="spinner"></span>
+                    </div>
+                    <span v-else>
+                        <slot>Send Message</slot>
+                    </span>
+                </transition>
             </button>
         </form>
     </div>
@@ -25,24 +32,30 @@ export default {
             name: '',
             email: '',
             message: '',
+            loading: false,
         }
     },
     methods:{
         async sendMessage(){
+            this.loading = true;
             try {
                 await emailjs.send('service_at8vndn', 'template_6jsm1mx',  {
                     message: this.message,
                     from_name: this.name,
                     reply_to: this.email
                 },
-                '2VemSJ1mMzEOw4YeW');
+                '2VemSJ1mMzEOw4YeW').then(()=> {
+                    this.$swal('Email Sent', 'Email successfully sent to Mehwish', 'success');
+                })
+
         } catch(error) {
-            console.log({error})
-         }
+            this.$swal('Error', 'Unable to send email. Please try again', 'error');
+        }
+        this.loading = false;
         // Reset form field
-        this.name = ''
-        this.email = ''
-        this.message = ''
+            this.name = ''
+            this.email = ''
+            this.message = ''
         }
     }
 }
@@ -87,6 +100,68 @@ export default {
             margin: 4rem auto 0;
             font-family: Roboto-bold;
             cursor: pointer;
+            .spinner {
+                display: inline-block;
+                margin: 0 8px;
+                border-radius: 50%;
+                width: 1.5em;
+                height: 1.5em;
+                border: .215em solid transparent;
+                vertical-align: middle;
+                font-size: 16px;
+                border-top-color: #0a192f;
+                animation: spin 1s cubic-bezier(.55, .15, .45, .85) infinite;
+            }
+            .v-btn-label-enter-active {
+                animation: slide-in-down 260ms cubic-bezier(.0, .0, .2, 1);
+            }
+
+            .v-btn-label-leave-active {
+                animation: slide-out-down 260ms cubic-bezier(.4, .0, 1, 1);
+            }
+
+
+            @keyframes spin {
+                0% {
+                    transform: rotate(0deg);
+                }
+
+                100% {
+                    transform: rotate(360deg);
+                }
+            }
+
+            @keyframes slide-in-down {
+                0% {
+                    transform: translateY(-20px);
+                    opacity: 0;
+                }
+
+                50% {
+                    opacity: .8;
+                }
+
+                100% {
+                    transform: translateY(0);
+                    opacity: 1;
+                }
+            }
+
+            @keyframes slide-out-down {
+                0% {
+                    transform: translateY(0);
+                    opacity: 1;
+                }
+
+                40% {
+                    opacity: .2;
+                }
+
+                100% {
+                    transform: translateY(20px);
+                    opacity: 0;
+                }
+            }
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
+const options = {
+    confirmButtonColor: '#0a192f',
+  };
 
 const app = createApp(App);
 app.use(Antd);
+app.use(VueSweetalert2, options);
 app.mount('#app')
